83cfb5fb1f11a39c14e46533d88db91b185a9685
pwdd <aina@pintowebdevelopment.com>
2015-07-09 17:15:50 -0700

    delete parallax on opening

--- assets/javascripts/js.js
+++ assets/javascripts/js.js
@@ -80,21 +80,6 @@ $(function() {
   });
 });
 
-$(function() {
-
-	$window = $(window);
-
-	$('section[data-type="background"]').each(function() {
-		var $bgobj = $(this);
-
-		$(window).scroll(function() {
-			var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
-			var coords = '50% '+ yPos + 'px';
-			$bgobj.css({ backgroundPosition: coords });
-		});
-	});	
-}); 
-
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
