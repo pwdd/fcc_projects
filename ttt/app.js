16dfbe8ebf18278fb01a860254ccfb4a93229a68
pwdd <aina@pintowebdevelopment.com>
2015-11-24 12:02:19 -0800

    render value of human player on click

--- ttt/app.js
+++ ttt/app.js
@@ -17,6 +17,41 @@ var Player = function(symbol, isComputer) {
   this.isComputer = isComputer;
 };
 
+var Cell = function(val, pos, css) {
+  this.val = val;
+  this.pos = pos;
+  this.css = css;
+};
+
+var Board = function() {
+  this.COMBOSIZE = 3;
+  this.BOARDSIZE = 9;
+
+  this.state = new Array(9);
+
+  this.board = [
+    new Cell("", 0, ["bb", "br"]), new Cell("", 1, ["bb", "br"]), new Cell("", 2, ["bb"]),
+    new Cell("", 3, ["bb", "br"]), new Cell("", 4, ["bb", "br"]), new Cell("", 5, ["bb"]),
+    new Cell("", 6, ["br"]),       new Cell("", 7, ["br"]),       new Cell("", 8, [])
+  ];
+
+  this.wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  this.canMove = function(pos) {
+    return this.state[pos] === undefined;
+  };
+};
 
 /*
  * app
@@ -25,7 +60,11 @@ var Player = function(symbol, isComputer) {
 (function() {
   var ttt = angular.module('tictactoe', []);
 
-  ttt.controller('GameController', ['$scope', function($scope) {
+  ttt.service('board', [Board]);
+
+  ttt.controller('GameController', ['$scope', 'board', function($scope, board) {
+    $scope.board = board.board;
+
     $scope.setPlayerSymbol = function(symbol) {
       if (symbol == 'x') {
         $scope.human = new Player('x', false);
@@ -36,7 +75,32 @@ var Player = function(symbol, isComputer) {
       };
     };
 
+    $scope.setCellVal = function(pos) {
+      if (board.canMove(pos)) {
+        $scope.board[pos].val = $scope.human.symbol;
+        board.state[pos] = -1;
+      };
+    };
+
   }]);
 
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
